a2a5c06ea23608ca9d1d7420e52162373c4b8478
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2021-03-19 00:34:33 -0400

    iteration 4 complete!

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,27 +1,25 @@
 /*
-Here i extended the branch style solution by nesting seperate branches for admin and standard user. This way only one of the two branches of code will run depending on whether or not someone is admin. 
+For the final iteration all i really had to do was change the required minimum within the regular expression check i put in place. Since i planned this out ahead i set the branches up to be easy to change.
 
-Admin is set to 'no' (as a string) by default, if 'yes' is added as a second argument it will run the password strength check for an admin level user. 
-
-The reason i didn't use the boolean value of true or false in this case is so that truthy or falsey values can't be passed in as an argument to get around the restrictions for an admin.
+Also i changed the argument name from admin to user , in order to be more in line with what the document asked for. So now in order to require admin credentials the second argument must be passed as admin.
 */
 
-const iteration3 = (password, admin = 'no') => {
-  if (admin === 'no') {
+const iteration4 = (password, user = 'normal') => {
+  if (user === 'normal') {
     if (!password.match(/[A-Z]{1}/i)) {
       return 'Password must contain at least 1 Letter'
     } else if (!password.match(/\d{1}/)) {
       return 'Password must contain at least 1 Number'
-    } else if (password.length < 8) {
-      return 'Password must be at least 8 characters long'
+    } else if (password.length < 10) {
+      return 'Password must be at least 10 characters long'
     }
-  } else if (admin === 'yes') {
+  } else if (user === 'admin') {
     if (!password.match(/[A-Z]{1}/i)) {
       return 'Password must contain at least 1 Letter'
     } else if (!password.match(/\d{1}/)) {
       return 'Password must contain at least 1 Number'
-    } else if (!password.match(/[!@#$%^&*]{1}/)) {
-      return 'Password must contain at least one of the following special characters ! @ # $ % ^ & *'
+    } else if (!password.match(/[!@#$%^&*]{3}/)) {
+      return 'Password must contain at least 3 of the following special characters ! @ # $ % ^ & *'
     } else if (password.length < 13) {
       return 'Password must be at least 13 characters long'
     }
@@ -29,8 +27,8 @@ const iteration3 = (password, admin = 'no') => {
   return 'Password Accepted!'
 }
 
-console.log(iteration3('hello!#$!@$@!', 'yes')) // Password must contain at least 1 Number
-console.log(iteration3('a12345!', 'yes')) //Password must be at least 13 characters long
-console.log(iteration3('141415!#@!#!@#132')) //Password must contain at least 1 Letter
-console.log(iteration3('hello133123124', 'yes')) //Password must contain at least one of the following special characters ! @ # $ % ^ & *
-console.log(iteration3('goodbye123')) //Password Accepted!
+console.log(iteration4('hello1!#$!@$@!', 'admin')) // Password Accepted!
+console.log(iteration4('a!@#12345!', 'admin')) //Password must be at least 13 characters long
+console.log(iteration4('141415!#@!#!@#132')) //Password must contain at least 1 Letter
+console.log(iteration4('hello!!3124', 'admin')) //Password must contain at least 3 of the following special characters ! @ # $ % ^ & *
+console.log(iteration4('goodbye123')) //Password Accepted!
